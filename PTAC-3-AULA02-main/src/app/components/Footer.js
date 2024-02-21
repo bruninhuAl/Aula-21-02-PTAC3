0cd7578f742517fa18972d216c9e20c53d57b1ec
bruninhuAl <brunoacristo13@gmail.com>
2024-02-21 17:14:26 -0400

    commit 21/02

---
+++ PTAC-3-AULA02-main/src/app/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <Footer>
+            <p>
+                THIS IS A REAL FOOTER CREATED BY BRUNO
+            </p>
+        </Footer>
+    )
+}
---
+++ PTAC-3-AULA02-main/src/app/components/Menu.js
@@ -0,0 +1,14 @@
+export default function Menu(){
+    return(
+        <header>
+            <h1></h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Registrar</li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
