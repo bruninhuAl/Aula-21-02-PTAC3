0eabb17ea5932f56c7e237f777c99da7f795c15d
bruninhuAl <brunoacristo13@gmail.com>
2024-02-21 17:26:08 -0400

    commitando pela segunda vez no mesmo dia

--- PTAC-3-AULA02-main/src/app/components/Menu.js
+++ PTAC-3-AULA02-main/src/app/components/Menu.js
@@ -1,11 +1,18 @@
+import link from "next/link"
 export default function Menu(){
     return(
         <header>
             <h1></h1>
             <nav>
                 <ul>
+                    <link href="/">
+
                     <li>Home</li>
+                    </link>
+
+                    <link href="/">
                     <li>Registrar</li>
+                    </link>
                 </ul>
             </nav>
         </header>
