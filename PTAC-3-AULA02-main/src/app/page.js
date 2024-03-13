4f66a7585cb761e9d1a595c749f1244aae92db84
bruninhuAl <brunoacristo13@gmail.com>
2024-03-13 17:23:17 -0400

    commit 13/03

--- PTAC-3-AULA02-main/src/app/page.js
+++ PTAC-3-AULA02-main/src/app/page.js
@@ -1,8 +1,22 @@
 export default async function Home() {
-
+  const resposta = await fetch("http://localhost:3000/api",{
+    next:{
+    revalidate: 1
+  }
+});
+const campus = await resposta.json();
     return (
       <main>
         <h1>Home</h1>
+        {
+          campus.map((campi) =>
+            <div>
+              <p>{campi.nome_campi}</p>
+            </div>
+
+
+          )
+      }
       </main>
-    );
+    )
 }
